44b578abf157e203cc9ca8321bade4bfaf7daefd
alaeddine-zeraib <alaeddine.zeraib@gmail.com>
2021-03-09 21:58:51 +0100

    translation

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ const signInButton = document.querySelector('#signInButton');
 const signUpButton = document.querySelector('#signUpButton');
 const overlayContainer = document.querySelector('.container .overlay-container');
 const overlay = document.querySelector('.container .overlay-container .overlay');
+const signInForm = document.querySelector('.container .sign-in-form');
+const signUpForm = document.querySelector('.container .sign-up-form');
 
-if(signInButton){
-    signInButton.addEventListener('click', () => {
-            overlayContainer.style.transform = 'translateX(100%)';
-    } , false);
-}
-// signInButton.addEventListener('click', () => {
-//     overlayContainer.style.transform = 'translateX(100%)';
-// });
+signInButton.addEventListener('click', () => {
+    overlayContainer.style.transform = 'translateX(100%)';
+    overlay.style.transform = 'translateX(-50%)';
+    signInForm.classList.add('active');
+});
+
+signUpButton.addEventListener('click', () => {
+    overlayContainer.style.transform = 'translateX(0%)';
+    overlay.style.transform = 'translateX(0%)';
+    signUpForm.classList.add('active');
+});
